95631f4a704385c9190225a4ae760e3c53001cec
Vivek <vivekmannava@gmail.com>
2021-07-02 12:05:12 +0530

    Initial upload

---
+++ Day 2/trial.py
@@ -0,0 +1,44 @@
+# print("Hello"[0:-1])
+
+# print("123" + "234")
+
+# print(123_234_123)
+
+# num_char = len(input("Enter your name : "))
+# print(" your name has ",num_char," characters")
+
+#----------------------------------------------------------------------------------
+
+# num_char =  str(num_char)
+# print(type(num_char))
+
+# two_digit_number = input("Please enter a 2 digit number : ")
+# digit_1 = two_digit_number[0]
+# digit_2 = two_digit_number[1]
+
+# print(int(digit_1) + int(digit_2))
+
+#----------------------------------------------------------------------------------
+
+# height = float(input('Please enter your height in metres : '))
+# weight = float(input("Please enter your weight in kilos  : "))
+
+# BMI = int(weight/(height ** 2))
+
+# print("your BMI is : ",BMI)
+
+#----------------------------------------------------------------------------------
+
+# age = int(input('What is your current age ? '))
+
+# years  = (90 - age)
+
+# months = years * 12
+ 
+# days = years * 365 
+
+# weeks = years * 52 
+
+# print(f"you've got {years} years left, which is roughly {months} months or {weeks} weeks. In days, that would be {days} days")
+
+#----------------------------------------------------------------------------------
