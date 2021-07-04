b11d7d267df1f786780994021ff5d523a17ac81c
Vivek <vivekmannava@gmail.com>
2021-07-04 21:20:19 +0530

    Updated Comments

--- Day 4/hand_cricket.py
+++ Day 4/hand_cricket.py
@@ -1,6 +1,7 @@
 import random
 import keyboard
 
+### Initialize the values. Keeping score as -1 to start off
 your_score = -1
 computer_score = -1
 Not_Out = True
@@ -8,7 +9,7 @@ Not_Out = True
 your_choice = 0
 computer_choice = 0
 
-
+### This function runs in a loop until the user gives a valid response. 
 
 def user_input(lowerend,upperend):
     while True:
@@ -22,6 +23,12 @@ def user_input(lowerend,upperend):
         else:
             print('Invalid Input Character.Please enter again.')
 
+### this function is called when the user is batting...Need to check the defend count as well for user. 
+
+### Function ends when either user defends more than 3 times in a row or
+### user is on the chase and scores more than the computer
+### user takes a double century
+### and obviously the computer bowls the same number as you
 
 def batting():
     your_batting_score = 0
@@ -31,6 +38,7 @@ def batting():
         print("Please enter a number between 1 and 6 to score and 0 to defend : ")
         your_runs = user_input(0,6)
         computer_runs = random.randint(1,6)
+        print(f"Computer chose to bowl a {computer_runs}\n")  
 
         if your_runs == computer_runs:
             print('You are out...Your score is ',your_batting_score)
@@ -48,18 +56,27 @@ def batting():
         your_batting_score += your_runs
 
         if your_batting_score > computer_score and computer_score != -1: 
+            print("Your score is ",your_batting_score)
             return your_batting_score
 
         if your_batting_score >= 200:
             print("200 has been reached..Sayonara")
             return your_batting_score
 
+
+### This function is called when the computer is batting
+
+### Defend score is not checked here as the computer is fearless and doesnt defend itself
+### The function ends when either the computer scores more than user or scores a double century 
+### and obviously when user bowls the same number as the computer
+
 def bowling():
     computer_batting_score = 0
     while True:
         print("Enter a number between 1 and 6 to bowl")
-        your_runs = user_input(0,6)
-        computer_runs = random.randint(1,6)   
+        your_runs = user_input(1,6)
+        computer_runs = random.randint(1,6) 
+        print(f"Computer chose to hit it for a {computer_runs}\n")  
 
         if computer_runs == your_runs:
             print("Computer is OUT! Computer's score is ",computer_batting_score)
@@ -68,6 +85,7 @@ def bowling():
         computer_batting_score += computer_runs 
 
         if computer_batting_score > your_score and your_score != -1:
+            print("Computer's score is ",computer_batting_score)
             return computer_batting_score
 
         if computer_batting_score >= 200:
@@ -75,7 +93,7 @@ def bowling():
             return computer_batting_score
 
 
-
+### Prompt user for either 1 or 2. 
 
 print("Time for the toss : Enter 1 for Odd and 2 for Even")
 odd_even = user_input(1,2)
@@ -92,20 +110,22 @@ computer_choice = random.randint(1,6)
 
 combo_odd_even = your_choice + computer_choice 
 
+### Based on the winner of the toss, decide who bats and who bowls first
+### if user choses even and the combo is even, or both are odd, then user won the toss. 
 
-
-if odd_even == 2 and combo_odd_even%2 == 0:
-    print("Choose : 1 for Batting and 2 for Bowling : ")
+if (odd_even == 2 and combo_odd_even%2 == 0) or (odd_even == 1 and combo_odd_even%2 == 1):
+    print("You won the Toss...Choose : 1 for Batting and 2 for Bowling : ")
     your_choice = user_input(1,2)
     if your_choice == 1 : 
         print("\n\nYou are now at the crease\n")
         your_score = batting()
-        print(f"\nComputer needs{your_score + 1} to win")
+        print(f"\nComputer needs {your_score + 1} to win")
         print("\n\nComputer is now at the crease\n")
         computer_score = bowling()
     else:
         print("Computer is now at the crease")
         computer_score = bowling()
+        print(f"You need {computer_score + 1} to win")
         print("You are now at the crease.")
         your_score = batting()
 else:
@@ -119,13 +139,15 @@ else:
     else:
         print("\n\nComputer won the toss and chose to Bowl first\n")
         your_score = batting()
+        print(f"\nComputer needs {your_score + 1} to win")
         print("\n\nComputer is now at the crease\n")
         computer_score = bowling()
 
 
 ### End of the odd or even loop. Now onto the scoring
 
-
+### To display the celebratory message : 
+### we dont ridicule people who scored nothing, so we dont check for 0.
 
 if your_score >= 200:
     print("Thats a double century for you..take a bow!")
@@ -142,5 +164,8 @@ else:
     print("\nCongratulations ... You Won!")
 
 print("Done...press escape to exit")
+
+### This was something new I learned especially for this ..!
+### otherwise the exe version just closes without giving user time to read the final verdict
 
 keyboard.wait('esc')
